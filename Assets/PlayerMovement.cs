799fc523171636a34bbcd9292aaec4c46a6a13c2
Niallhampton <2501302@uad.ac.uk>
2025-12-16 10:27:29 +0000

    scripts (not already committed to main??)

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class AutoRunner : MonoBehaviour
+{
+    public float speed = 6f;
+    public float jumpForce = 10f;
+
+    private Rigidbody2D rb;
+    private bool isGrounded;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // Always move right
+        rb.linearVelocity = new Vector2(speed, rb.linearVelocity.y);
+
+        // Jump (one button)
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        isGrounded = true;
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        isGrounded = false;
+    }
+}
